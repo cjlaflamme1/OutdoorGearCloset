851ce917745d97e1ae0116e397a250d33c4abf78
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-11-27 12:36:06 -0500

    removing hello world

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -7,10 +7,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // @Get()
-  // getHello() {
-  //   return this.appService.getHello();
-  // }
+
 
   }
 
