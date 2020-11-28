03de33fa0806d7443c3832eb0adb87f9ced0e6b9
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-11-27 21:06:08 -0500

    basic nav rendered, home removed

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigation/navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -16,7 +15,6 @@ import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     NavigationComponent
   ],
   imports: [
