9a62952aeb658aaca2c5f1ea9a6ec4917659eb01
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-11-28 19:33:10 -0500

    dynamic column grid

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  breakpoint;
   constructor() { }
 
   ngOnInit(): void {
+    this.breakpoint = (window.innerWidth <= 700) ? 1 : 2;
+  }
+
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 700) ? 1 : 2;
   }
 
 }
