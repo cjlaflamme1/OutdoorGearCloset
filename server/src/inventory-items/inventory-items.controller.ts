220cbd95cdc2f217d14c107ab19cf5671fafdfe7
Chad Laflamme <cjlaflamme1@plymouth.edu>
2021-01-14 11:00:50 -0500

    inventory item repository service and control complete

--- server/src/inventory-items/inventory-items.controller.ts
+++ server/src/inventory-items/inventory-items.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateItemDTO } from './dto/create-inventory-item.dto';
+import { InventoryItem } from './inventory-item.entity';
+import { InventoryItemsService } from './inventory-items.service';
 
-@Controller('inventory-items')
-export class InventoryItemsController {}
+@Controller('api/inventoryitems')
+export class InventoryItemsController {
+    constructor(private readonly inventoryItemsService: InventoryItemsService) {}
+
+    @Post()
+    create(@Body() createItemDTO: CreateItemDTO): Promise<InventoryItem> {
+      return this.inventoryItemsService.create(createItemDTO);
+    }
+  
+    @Get()
+    findAll(): Promise<InventoryItem[]> {
+      return this.inventoryItemsService.findAll();
+    }
+  
+    @Get(':id')
+    findOne(@Param('id') id: string): Promise<InventoryItem> {
+      return this.inventoryItemsService.findOne(id);
+    }
+  
+    @Delete(':id')
+    remove(@Param('id') id: string): Promise<void> {
+      return this.inventoryItemsService.remove(id);
+    }
+}
